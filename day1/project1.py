5209726ffe8dce9c4704eb8f6511c031107d5605
乐思宇 <lesiyu129@163.com>
2024-07-08 10:00:36 +0800

    Add images and configure Black code formatterThis commit adds忽略jpg和png图片的规则到.gitignore文件中。同时，在.idea/misc.xml中配置了Black代码格式化工具，启用了在格式化和保存时的自动应用。此外，还对day1/project1.py中的代码进行了格式化，增加了读取并显示图片的代码，并将显示的窗口标题改为"image"。

--- day1/project1.py
+++ day1/project1.py
@@ -1,6 +1,8 @@
+# opencv读写图像
 import cv2
 
-img = cv2.imread('../data/lena.jpg')
-cv2.imshow('image', img)
+img = cv2.imread("../data/lena.jpg")
+cv2.imshow("image", img)
 cv2.waitKey()
 cv2.destroyAllWindows()
+cv2.imwrite("./lena.jpg", img)
